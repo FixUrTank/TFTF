a50a7b521f23247318a7519dcb4dcc5139cafc84
FixUrTank <savvynsleepy@gmail.com>
2018-04-22 16:09:34 -0800

    whole bunch of stuff

---
+++ buggy.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>TFTF</title>
+        <link rel="stylesheet" type="text/css" href="buggy.css">   
+    </head>
+    
+    <body>
+        <?php include 'homepage.php' ;?>
+
+
+    </body>
+</html>    
